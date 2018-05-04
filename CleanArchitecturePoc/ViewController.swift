f2eb5540368abfd4bf7494e58931a0fa3423648a
Willian Policiano <willian.policiano@itau-unibanco.com.br>
2018-05-04 07:50:52 -0300

    Initial Commit

---
+++ CleanArchitecturePoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitecturePoc
+//
+//  Created by Willian Fagner Souza Polici on 04/05/18.
+//  Copyright © 2018 Willian Fagner Souza Polici. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
